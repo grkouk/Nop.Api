4cc732d879ede97db61e7586180b8996cebf853b
GrKouk <grkouk@hotmail.com>
2019-11-12 13:13:19 +0200

    Small changes

--- GrKouk.Nop.Api/Startup.cs
+++ GrKouk.Nop.Api/Startup.cs
@@ -29,7 +29,7 @@ namespace GrKouk.Nop.Api
         {
             services.AddDbContext<ApiDbContext>(options =>
                 options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
